90f92f4a53c6c49422c7b76b8be937b314e09703
Nikola <nikolalatincic19@gmail.com>
2024-02-22 22:18:23 +0100

    Immediately Invoked Function Expressions, Closures, Challenge #2

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,94 @@
 'use strict';
 
+/////////////////////////////////
+// Closure examples
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Re-assigning f function
+h();
+f();
+
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+/*
+/////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+*/
+
+/*
+/////////////////////////////////
+// Immidiately Invoked Function Expression (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
+*/
+/*
 ////////////////////////////////
 // Call method
 
@@ -99,7 +188,7 @@ const tax = function (tax) {
 const addVAT2 = tax(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
-
+*/
 /*
 ////////////////////////////////
 // Functions returning Functions
